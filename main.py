e4457bd1c3b9007c13452d55a4b82a716f722427
cesarlabarca <cesarm.labarca@gmail.com>
2026-06-23 16:31:38 -0400

    remove a game

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 webhook = os.environ["WEBHOOK_URL"]
 
-games = {"storeID": 1,"steamAppID":"1593500,2322010,3357650,1984270,2352620" }
+games = {"storeID": 1,"steamAppID":"1593500,2322010,3357650,1984270" }
 r = requests.get("https://www.cheapshark.com/api/1.0/deals",params= games)
 r = r.json()
 
@@ -14,7 +14,6 @@ hours ={
   "2322010" : 41,
   "3357650" : 16,
   "1984270" : 46,
-  "2352620" : 30
 
 }
 
@@ -24,11 +23,9 @@ def gamesale(game,hours):
       onSale = i["isOnSale"]
       if onSale == "1":
         id = i["steamAppID"]
-        print(id)
         hrs = hours[f"{id}"]
         title = i["title"]
         price = i["salePrice"]
-        print(type(price))
         equation = round(float(price) / hrs,2)
         videogame = f"{title} = {price}  / price / hrs : {equation}\n"
         master += videogame
